7fadf3d59be37f12a930cfc4ca259767fb78f75c
P. Siegl <ps.port@gmx.de>
2016-03-30 09:46:53 +0200

    updated kernel_sse_fma

--- bench.py
+++ bench.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import sys
 import operator
+import multiprocessing
 
 kernel = "seismic.elf"
 
@@ -23,11 +24,14 @@ for var in out.decode("utf-8").splitlines():
   nums = 10
   sum_ms = 0.0
   for t in range(0,nums):
-    cmd = "./%s --timesteps=1000 --width=2000 --height=516 --pulseX=600 --pulseY=70 --threads=4 --kernel=%s" % (kernel, var)
+    cmd = "./%s --timesteps=1000 --width=2000 --height=516 --pulseX=600 --pulseY=70 --threads=%d --kernel=%s" % (kernel, multiprocessing.cpu_count(), var)
     out2, err2 = subprocess.Popen( cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE ).communicate()
     sum_ms += float( ("%s" % out2.decode("utf-8").splitlines()[20]).split(" ")[-1].replace(")","") )
   res[ var ] = sum_ms/float(nums)
+  print("%s: done" % var )
 
+print("")
+print("results:")
 sorted_res = sorted(res.items(), key=operator.itemgetter(1))
 for t in sorted_res:
   print("%.2f: %s" % (t[1], t[0]))
--- kernel_sse_fma.c
+++ kernel_sse_fma.c
@@ -251,31 +251,38 @@ inline __attribute__((always_inline)) void kernel_sse_fma_partial_aligned( stack
     __m128 s_ppf_aligned, s_vel_aligned, s_actual, s_above1, s_left1, s_under1, s_right1, s_sum1;
     __m128 s_above2, s_under2, s_left2, s_right2;
 
-    unsigned i, j;
-    // spatial loop in x
-    for (i=data->x_start; i<data->x_end; i++) {
+    unsigned len_x = data->x_end - data->x_start;
+    unsigned len_y = (data->y_end - data->y_start)/4;
 
-        unsigned r = i * data->height + data->y_start;
-        s_above2 = _mm_loadu_ps( &(data->apf[ r -2]) );
+    unsigned i, j;
+    unsigned r = data->x_start * data->height + data->y_start;
+    float * NPPF = &data->nppf[ r ];
+    float * VEL = &data->vel[ r ];
+    float * APF = &data->apf[ r ];
+    float * APF_pl1 = APF + data->height;
+    float * APF_pl2 = APF_pl1 + data->height;
+    float * APF_min1 = APF - data->height;
+    float * APF_min2 = APF_min1 - data->height;
+    APF += 2;
+
+    // spatial loop in x
+    for (i=0; i<len_x; i++) {
+
+        s_above2 = _mm_loadu_ps( APF - 4 ); // APF - 2
 
         // spatial loop in y
-        for (j=data->y_start; j<data->y_end; j+=4) {
-            r = i * data->height + j;
-            unsigned r_min1 = r - data->height;
-            unsigned r_min2 = r - (data->height * 2);
-            unsigned r_plus1 = r + data->height;
-            unsigned r_plus2 = r + (data->height * 2);
-            
+        for (j=0; j<len_y; j++) {
+
             // calculates the pressure field t+1
-            s_ppf_aligned = _mm_load_ps( &(data->nppf[ r ]) ); // align it to get _load_ps
-            s_vel_aligned = _mm_load_ps( &(data->vel[ r ]) );
+            s_ppf_aligned = _mm_load_ps( NPPF ); // align it to get _load_ps
+            s_vel_aligned = _mm_load_ps( VEL );
 
-            s_left1 = _mm_load_ps( &(data->apf[ r_min1 ]) );
-            s_left2 = _mm_load_ps( &(data->apf[ r_min2 ]) );
-            s_right2 = _mm_load_ps( &(data->apf[ r_plus2 ]) );
-            s_right1 = _mm_load_ps( &(data->apf[ r_plus1 ]) );
+            s_left1 = _mm_load_ps( APF_min1 );
+            s_left2 = _mm_load_ps( APF_min2 );
+            s_right2 = _mm_load_ps( APF_pl2);
+            s_right1 = _mm_load_ps( APF_pl1 );
 
-            s_under2 = _mm_loadu_ps( &(data->apf[ r +2]) );
+            s_under2 = _mm_loadu_ps( APF ); // APF + 2
 
             s_actual = _mm_shuffle_ps( s_above2, s_under2, _MM_SHUFFLE( 1, 0, 3, 2 ) ); // 3 4 5 6
             s_above1 = _mm_shuffle_ps( s_above2, s_actual, _MM_SHUFFLE( 2, 1, 2, 1 ) ); // 2 3 4 5
@@ -292,10 +299,24 @@ inline __attribute__((always_inline)) void kernel_sse_fma_partial_aligned( stack
             s_sum1 = _mm_fnmadd_ps( s_sixty, s_actual, s_sum1 );
             s_sum1 = _mm_fmadd_ps( s_vel_aligned, s_sum1, _mm_fmsub_ps(s_two, s_actual, s_ppf_aligned) );
 
-            _mm_store_ps( &(data->nppf[ r ]), s_sum1);
-            
+            _mm_store_ps( NPPF, s_sum1);
+
             s_above2 = s_under2;
+            NPPF+=4;
+            VEL+=4;
+            APF+=4;
+            APF_pl1+=4;
+            APF_pl2+=4;
+            APF_min1+=4;
+            APF_min2+=4;
         }
+        APF+=4;
+        NPPF+=4;
+        VEL+=4;
+        APF_min1+=4;
+        APF_min2+=4;
+        APF_pl1+=4;
+        APF_pl2+=4;
     }
 }
 
