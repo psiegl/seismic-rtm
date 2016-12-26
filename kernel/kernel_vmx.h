e9570ac829f6f6093d77f28c39306830bea1cf89
P. Siegl <ps.port@gmx.de>
2016-12-25 20:08:39 -0500

    re-added PPC port

---
+++ kernel/kernel_vmx.h
@@ -0,0 +1,184 @@
+//  This file is part of seismic-rtm.
+//
+//  seismic-rtm is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  seismic-rtm is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with seismic.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _KERNEL_VMX_H_
+#define _KERNEL_VMX_H_
+#ifdef __ALTIVEC__
+#include "kernel.h"
+#include <altivec.h>
+
+// function that implements the kernel of the seismic modeling algorithm
+#define SEISMIC_EXEC_PPC_FCT( NAME ) \
+void seismic_exec_##NAME( void* v ) \
+{ \
+    stack_t* data = (stack_t*) v; \
+ \
+    /* preload register with const. values. */ \
+    vector float s_two = (vector float){2.0f, 2.0f, 2.0f, 2.0f}; \
+    vector float s_sixteen = (vector float){16.0f,16.0f,16.0f,16.0f}; \
+    vector float s_sixty = (vector float){-60.0f,-60.0f,-60.0f,-60.0f}; \
+ \
+    /* permutation masks */ \
+    vector unsigned char mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow; \
+    mergeOneHighThreeLow = (vector unsigned char) \
+        { 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B }; \
+    mergeThreeHighOneLow = (vector unsigned char) \
+        { 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13 }; \
+    mergeHighLow = (vector unsigned char) \
+        { 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 }; \
+ \
+    data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[0]; \
+ \
+    unsigned num_div = data->timesteps / 10; \
+    unsigned num_mod = data->timesteps - (num_div * 10); \
+ \
+    gettimeofday(&data->s, NULL); \
+ \
+    /* time loop */ \
+    unsigned t, r, t_tmp = 0; \
+    for( r = 0; r < 10; r++ ) { \
+        for (t = 0; t < num_div; t++, t_tmp++) \
+        { \
+            kernel_##NAME( data, s_two, s_sixteen, s_sixty, mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow ); \
+ \
+            /* switch pointers instead of copying data */ \
+            float * tmp = data->nppf; \
+            data->nppf = data->apf; \
+            data->apf = tmp; \
+ \
+            /* + 1 because we add the pulse for the _next_ time step */ \
+            /* inserts the seismic pulse value in the desired position */ \
+            data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[t_tmp+1]; \
+        } \
+ \
+        /* shows one # at each 10% of the total processing time */ \
+        { \
+            printf("#"); \
+            fflush(stdout); \
+        } \
+    } \
+    for (t = 0; t < num_mod; t++) \
+    { \
+        kernel_##NAME( data, s_two, s_sixteen, s_sixty, mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow ); \
+ \
+        /* switch pointers instead of copying data */ \
+        float * tmp = data->nppf; \
+        data->nppf = data->apf; \
+        data->apf = tmp; \
+ \
+        /* + 1 because we add the pulse for the _next_ time step */ \
+        /* inserts the seismic pulse value in the desired position */ \
+        data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[t_tmp+t+1]; \
+    } \
+ \
+    gettimeofday(&data->e, NULL); \
+} \
+ \
+ \
+void seismic_exec_##NAME##_pthread(void * v ) \
+{ \
+    stack_t * data = (stack_t*) v; \
+ \
+    /* preload register with const. values. */ \
+    vector float s_two = (vector float){2.0f, 2.0f, 2.0f, 2.0f}; \
+    vector float s_sixteen = (vector float){16.0f,16.0f,16.0f,16.0f}; \
+    vector float s_sixty = (vector float){-60.0f,-60.0f,-60.0f,-60.0f}; \
+ \
+    /* permutation masks */ \
+    vector unsigned char mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow; \
+    mergeOneHighThreeLow = (vector unsigned char) \
+        { 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B }; \
+    mergeThreeHighOneLow = (vector unsigned char) \
+        { 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13 }; \
+    mergeHighLow = (vector unsigned char) \
+        { 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17 }; \
+ \
+    if( data->set_pulse ) \
+        data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[0]; \
+ \
+    unsigned num_div = data->timesteps / 10; \
+    unsigned num_mod = data->timesteps - (num_div * 10); \
+ \
+    /* start everything in parallel */ \
+    BARRIER( data->barrier, data->id ); \
+ \
+    gettimeofday(&data->s, NULL); \
+ \
+    /* time loop */ \
+    unsigned t; \
+    if( data->set_pulse ) \
+    { \
+        unsigned r, t_tmp = 0; \
+        for( r = 0; r < 10; r++ ) { \
+            for (t = 0; t < num_div; t++, t_tmp++) \
+            { \
+                kernel_##NAME( data, s_two, s_sixteen, s_sixty, mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow ); \
+ \
+                /* switch pointers instead of copying data */ \
+                float * tmp = data->nppf; \
+                data->nppf = data->apf; \
+                data->apf = tmp; \
+ \
+                /* + 1 because we add the pulse for the _next_ time step */ \
+                /* inserts the seismic pulse value in the desired position */ \
+                data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[t_tmp+1]; \
+ \
+                BARRIER( data->barrier, data->id ); \
+            } \
+ \
+            /* shows one # at each 10% of the total processing time */ \
+            { \
+                printf("#"); \
+                fflush(stdout); \
+            } \
+        } \
+        for (t = 0; t < num_mod; t++) \
+        { \
+            kernel_##NAME( data, s_two, s_sixteen, s_sixty, mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow ); \
+ \
+            /* switch pointers instead of copying data */ \
+            float * tmp = data->nppf; \
+            data->nppf = data->apf; \
+            data->apf = tmp; \
+ \
+            /* + 1 because we add the pulse for the _next_ time step */ \
+            /* inserts the seismic pulse value in the desired position */ \
+            data->apf[data->x_pulse * data->height + data->y_pulse] += data->pulsevector[t_tmp+t+1]; \
+ \
+            BARRIER( data->barrier, data->id ); \
+        } \
+    } \
+    else \
+        for (t = 0; t < data->timesteps; t++) \
+        { \
+            kernel_##NAME( data, s_two, s_sixteen, s_sixty, mergeOneHighThreeLow, mergeThreeHighOneLow, mergeHighLow ); \
+ \
+            /* switch pointers instead of copying data */ \
+            float * tmp = data->nppf; \
+            data->nppf = data->apf; \
+            data->apf = tmp; \
+ \
+            BARRIER( data->barrier, data->id ); \
+        } \
+ \
+    gettimeofday(&data->e, NULL); \
+ \
+    if( data->id ) \
+        pthread_exit( NULL ); \
+}
+
+
+#endif /* #ifdef __ALTIVEC__ */
+#endif /* #ifndef _KERNEL_VMX_H_ */
