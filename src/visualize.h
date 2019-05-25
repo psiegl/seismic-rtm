7a39fd6b3c9a08be2e2596b80694ae62c8043525
Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2019-05-25 18:53:24 +0200

    Added lgtm grading

--- src/visualize.h
+++ src/visualize.h
@@ -1,7 +1,12 @@
 // Copyright 2017 - , Dr.-Ing. Patrick Siegl
 // SPDX-License-Identifier: BSD-2-Clause
 
+#ifndef _VISUALIZE_H_
+#define _VISUALIZE_H_
+
 #include "config.h"
 
 void write_matrice( config_t * config, float * apf, float * nppf  );
 void show_ascii( config_t * config, unsigned scale, float * apf, float * nppf  );
+
+#endif /* #ifndef _VISUALIZE_H_ */
