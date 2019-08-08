0830afb6a0667919abeadc356606062ffd77d0fb
Dr.-Ing. Patrick Siegl <patrick@siegl.it>
2019-08-08 09:49:29 +0200

    Update check_hw.h

--- src/check_hw.h
+++ src/check_hw.h
@@ -1,5 +1,5 @@
-// Copyright 2017 - , Dr.-Ing. Patrick Siegl
 // SPDX-License-Identifier: BSD-2-Clause
+// SPDX-FileCopyrightText: 2017 Dr.-Ing. Patrick Siegl patrick@siegl.it
 
 #ifndef _CHECK_HW_H_
 #define _CHECK_HW_H_
