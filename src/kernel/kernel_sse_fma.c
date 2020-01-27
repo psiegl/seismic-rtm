8f9404b628dc593b9ba2b03b4f9a69b14ba6e14d
Dr.-Ing. Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2020-01-27 14:53:05 +0100

    avx_unaligned is now bin.-compatible

--- src/kernel/kernel_sse_fma.c
+++ src/kernel/kernel_sse_fma.c
@@ -6,50 +6,81 @@
 
 inline __attribute__((always_inline)) void kernel_sse_fma_std( stack_t * data, __m128 s_two, __m128 s_sixteen, __m128 s_sixty  )
 {
-    __m128 s_ppf_aligned, s_vel_aligned, s_actual, s_above1, s_left1, s_under1, s_right1, s_sum1;
+    __m128 s_ppf_aligned, s_vel_aligned, s_actual, s_above1, s_left1, s_under1, s_right1, s_sum;
     __m128 s_above2, s_under2, s_left2, s_right2;
     
+#if 0
+    float min1f[4] = { -1, -1, -1, -1 };
+    __m128 min1v = _mm_loadu_ps( min1f );
+    __m128 min_sixty_v = _mm_mul_ps( s_sixty, min1v );
+#endif
+
     unsigned i, j;
     // spatial loop in x
     for (i=data->x_start; i<data->x_end; i++) {
         // spatial loop in y
-        for (j=data->y_start; j<data->y_end; j+=4) {
+        for (j=data->y_start; j<data->y_end; j+=data->y_offset) {
             unsigned r = i * data->height + j;
             unsigned r_min1 = r - data->height;
-            unsigned r_min2 = r - (data->height * 2);
+            unsigned r_min2 = r - (data->height << 1);
             unsigned r_plus1 = r + data->height;
-            unsigned r_plus2 = r + (data->height * 2);
-            
+            unsigned r_plus2 = r + (data->height << 1);
+
             // calculates the pressure field t+1
             s_ppf_aligned = _mm_load_ps( &(data->nppf[ r ]) ); // align it to get _load_ps
             s_vel_aligned = _mm_load_ps( &(data->vel[ r ]) );
-            s_actual = _mm_load_ps( &(data->apf[ r ]) );
 
+            s_above2 = _mm_loadu_ps( &(data->apf[ r - 2]) );
+            s_under2 = _mm_loadu_ps( &(data->apf[ r + 2]) );
             s_left1 = _mm_load_ps( &(data->apf[r_min1]) );
             s_left2 = _mm_load_ps( &(data->apf[r_min2]) );
             s_right2 = _mm_load_ps( &(data->apf[r_plus2]) );
             s_right1 = _mm_load_ps( &(data->apf[r_plus1]) );
-            s_above1 = _mm_loadu_ps( &(data->apf[ r -1]) );
-            s_under1 = _mm_loadu_ps( &(data->apf[ r +1]) );
 
-            s_above2 = _mm_loadl_pi( _mm_shuffle_ps(s_actual, s_actual, _MM_SHUFFLE(1, 0, 0, 0)),
-                                     (__m64 const*)&(data->apf[ r -2]));
+//           |0 1 2 3|4 5 6 7|
+//               |2 3 4 5|
+            s_actual = _mm_shuffle_ps( s_above2, s_under2, _MM_SHUFFLE( 1, 0, 3, 2 ) );
 
-            s_under2 = _mm_loadh_pi( _mm_shuffle_ps(s_actual, s_actual, _MM_SHUFFLE(0, 0, 3, 2)),
-                                     (__m64 const*)&(data->apf[ r +4]));
+//           |0 1 2 3|4 5 6 7|
+//               |2 3 4 5|
+//             |1 2 3 4|
+            s_above1 = _mm_shuffle_ps( s_above2, s_actual, _MM_SHUFFLE( 2, 1, 2, 1 ) );
 
-            // sum up
-            s_sum1 = _mm_add_ps( _mm_add_ps( s_above1, s_under1),
+//           |0 1 2 3|4 5 6 7|
+//               |2 3 4 5|
+//                 |3 4 5 6|
+            s_under1 = _mm_shuffle_ps( s_actual, s_under2, _MM_SHUFFLE( 2, 1, 2, 1 ) );
+
+#if 0
+            s_sum = _mm_add_ps( _mm_fmsub_ps( s_two,
+                                                s_actual,
+                                            s_ppf_aligned ),
+                                _mm_mul_ps( s_vel_aligned,
+                                            _mm_sub_ps( _mm_add_ps( _mm_mul_ps( min_sixty_v,
+                                                                                s_actual ),
+                                                                    _mm_mul_ps( s_sixteen,
+                                                                                _mm_add_ps( _mm_add_ps( _mm_add_ps( s_above1,
+                                                                                                                    s_under1 ),
+                                                                                                        s_left1 ),
+                                                                                            s_right1 ) ) ),
+                                                        _mm_add_ps( _mm_add_ps( _mm_add_ps( s_above2,
+                                                                                            s_under2 ),
+                                                                                s_left2 ),
+                                                                    s_right2 ) ) ) );
+
+
+#else
+            s_sum = _mm_add_ps( _mm_add_ps( s_above1, s_under1),
                                  _mm_add_ps( s_left1, s_right1));
 
             s_above2 = _mm_add_ps( _mm_add_ps( s_right2, s_left2),
                                    _mm_add_ps( s_under2, s_above2));
 
-            s_sum1 = _mm_fmsub_ps( s_sixteen, s_sum1,  s_above2);
-            s_sum1 = _mm_fnmadd_ps( s_sixty, s_actual, s_sum1 );
-            s_sum1 = _mm_fmadd_ps( s_vel_aligned, s_sum1, _mm_fmsub_ps(s_two, s_actual, s_ppf_aligned) );
-
-            _mm_store_ps( &(data->nppf[ r ]), s_sum1);
+            s_sum = _mm_fmsub_ps( s_sixteen, s_sum,  s_above2);
+            s_sum = _mm_fnmadd_ps( s_sixty, s_actual, s_sum );
+            s_sum = _mm_fmadd_ps( s_vel_aligned, s_sum, _mm_fmsub_ps(s_two, s_actual, s_ppf_aligned) );
+#endif
+            _mm_store_ps( &(data->nppf[ r ]), s_sum);
         }
     }
 }
