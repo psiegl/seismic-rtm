aa9d04639bd5b473552577ede7e16ba140788131
Dr.-Ing. Patrick Siegl <3261314+psiegl@users.noreply.github.com>
2020-01-27 14:53:05 +0100

    avx_unaligned is now bin.-compatible

--- src/kernel/kernel_avx.c
+++ src/kernel/kernel_avx.c
@@ -5,8 +5,12 @@
 
 inline __attribute__((always_inline)) void kernel_avx_unaligned( stack_t * data, __m256 s_two, __m256 s_sixteen, __m256 s_sixty )
 {
-    __m256 s_ppf_aligned, s_vel_aligned, s_actual, s_above1, s_left1, s_under1, s_right1, s_sum1;
+    __m256 s_ppf_aligned, s_vel_aligned, s_actual, s_above1, s_left1, s_under1, s_right1, s_sum;
     __m256 s_above2, s_under2, s_left2, s_right2;
+    
+    float min1f[8] = { -1, -1, -1, -1, -1, -1, -1, -1 };
+    __m256 min1v = _mm256_loadu_ps( min1f );
+    __m256 min_sixty_v = _mm256_mul_ps( s_sixty, min1v );
 
     unsigned i, j;
     // spatial loop in x
@@ -32,19 +36,25 @@ inline __attribute__((always_inline)) void kernel_avx_unaligned( stack_t * data,
             s_under1 = _mm256_loadu_ps( &(data->apf[ r +1]) );
 
             s_above2 = _mm256_loadu_ps( &(data->apf[ r -2]) );
-
             s_under2 = _mm256_loadu_ps( &(data->apf[ r +2]) );
 
-            // sum up
-            s_sum1 = _mm256_add_ps( _mm256_add_ps( s_under1, s_above1),
-                                    _mm256_add_ps( s_left1, s_right1));
-            s_above2 = _mm256_add_ps( _mm256_add_ps( s_right2, s_left2),
-                                      _mm256_add_ps( s_under2, s_above2));
-            s_sum1 = _mm256_mul_ps( s_sixteen, s_sum1 );
-            s_sum1 = _mm256_sub_ps( _mm256_sub_ps( s_sum1,  s_above2), _mm256_mul_ps( s_sixty, s_actual ) );
-            s_sum1 = _mm256_add_ps( _mm256_mul_ps( s_vel_aligned, s_sum1), _mm256_sub_ps(_mm256_mul_ps( s_two, s_actual ), s_ppf_aligned) );
+            s_sum = _mm256_add_ps( _mm256_sub_ps( _mm256_mul_ps( s_two,
+                                                                 s_actual ),
+                                                  s_ppf_aligned ),
+                                   _mm256_mul_ps( s_vel_aligned,
+                                                  _mm256_sub_ps( _mm256_add_ps( _mm256_mul_ps( min_sixty_v,
+                                                                                               s_actual ),
+                                                                                _mm256_mul_ps( s_sixteen,
+                                                                                               _mm256_add_ps( _mm256_add_ps( _mm256_add_ps( s_above1,
+                                                                                                                                            s_under1 ),
+                                                                                                                             s_left1 ),
+                                                                                                              s_right1 ) ) ),
+                                                                 _mm256_add_ps( _mm256_add_ps( _mm256_add_ps( s_above2,
+                                                                                                              s_under2 ),
+                                                                                               s_left2 ),
+                                                                                s_right2 ) ) ) );
 
-            _mm256_storeu_ps( &(data->nppf[ r ]), s_sum1);
+            _mm256_storeu_ps( &(data->nppf[ r ]), s_sum);
         }
     }
 }
